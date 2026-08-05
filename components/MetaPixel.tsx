dc6f7d3e57c6f5f5588a7a2b734e2798dbdc0afe
ardians45 <ardians1945@gmail.com>
2026-08-05 16:20:38 +0700

    fix: update Meta Pixel ID to 4422552327987552

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-export const FB_PIXEL_ID = "1773097447160733";
+export const FB_PIXEL_ID = "4422552327987552";
 
 // Helper to trigger custom pixel events manually
 export const trackPixelEvent = (name: string, options = {}) => {
